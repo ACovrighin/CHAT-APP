adde25e3212081ee541489bb1a20ad6339130c0c
ACovrighin <andrew44291@gmail.com>
2024-12-09 23:41:58 -0500

    Created User schema, added api for user profile pics

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,22 @@
-const express = require ("express")
+import express from "express";
+import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.routes.js" 
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000,() => console.log('Server Running on port 5000'));
+dotenv.config();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes)
+
+
+//ROUTES
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`)
+});
